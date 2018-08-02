0f0db52d7794d1a74220175f8a249e312e372fcd
chase <943616235@qq.com>
2018-08-02 22:20:48 +0800

    会员中心首页

---
+++ app.js
@@ -0,0 +1,29 @@
+//app.js
+var qcloud = require('./vendor/wafer2-client-sdk/index')
+var config = require('./config')
+
+App({
+    onLaunch: function () {
+        qcloud.setLoginUrl(config.service.loginUrl)
+    },
+    onShow: function() {
+        console.log('on show')
+    },
+    onHide: function() {
+       console.log('on hide') 
+    }
+})
+
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 20 * (clientWidth / 320) + 'px';
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
---
+++ pages/ucenter/index.js
@@ -0,0 +1,37 @@
+//index.js
+var qcloud = require('../../vendor/wafer2-client-sdk/index')
+var config = require('../../config')
+var util = require('../../utils/util.js')
+
+Page({
+    data: {
+        userInfo: {
+            tel: '138xxxx1234',
+            name: '测试',
+            imgUrl: '../../static/images/ucenter/1608185570311.jpg'
+        },
+        menuList: [
+            {
+                linkUrl: '',
+                icon: '../../static/images/ucenter/icon-1.png',
+                text: '我的行程'
+            },
+            {
+                linkUrl: '',
+                icon: '../../static/images/ucenter/icon-2.png',
+                text: '我的钱包'
+            },
+            {
+                linkUrl: '',
+                icon: '../../static/images/ucenter/icon-3.png',
+                text: '客服中心'
+            }
+        ],
+        switchBtn: '../../static/images/ucenter/icon-4.png',
+        logged: false,
+        takeSession: false,
+        requestResult: ''
+    },
+
+
+});
