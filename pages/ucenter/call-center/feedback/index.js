472f2e5a351623a8dc1bd6999def753793068a48
chase <943616235@qq.com>
2018-08-07 21:28:45 +0800

    客服中心  意见反馈

---
+++ pages/ucenter/call-center/feedback/index.js
@@ -0,0 +1,11 @@
+
+
+Page({
+    data: {
+        siteIcon: '../../../../static/images/ucenter/icon-25.png',
+        cameraIcon1: '../../../../static/images/ucenter/icon-23.png',
+        cameraIcon2: '../../../../static/images/ucenter/icon-24.png',
+        confirmIcon: '../../../../static/images/ucenter/icon-26.png',
+    },
+
+});
